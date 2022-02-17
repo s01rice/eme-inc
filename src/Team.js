bfd41a405fcd80a7d4e438c750787ac6ed7ef3c1
FirzenLovemore <jeremyhuang@Jeremys-MacBook-Pro.local>
2022-02-17 13:38:38 -0700

    add in description

--- src/Team.js
+++ src/Team.js
@@ -7,11 +7,11 @@ export const Team = [
     {
         title: 'chromedoves',
         url: '#',
-        desc: 'Ex-Goldman Sachs investment banker. Playing with bubbles since I was 5. Have fun with it – otherwise, what\'s the point?',
+        desc: 'Ex-Goldman Sachs investment banker. Playing with bubbles since I was 5. Laying down on brick at a time. Have fun with it – otherwise, what\'s the point?',
     },
     {
         title: 'akilwrath',
         url: '#',
-        desc: 'Ex-FAANG, Unicorn Engineer. Used to building systems, currently building a vibe. Mom does not approve.',
+        desc: 'Ex-FAANG, startup Engineer. Used to building systems, currently building a vibe. Mom does not approve.',
     },
 ]
