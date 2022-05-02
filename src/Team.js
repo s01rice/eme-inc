ff8a1cbf3f247d5334bcad2b006e13da41eea632
vampire hunter <vampirehunter@vampires-MacBook-Pro.local>
2022-05-01 21:34:25 -0700

    Update name and description

--- src/Team.js
+++ src/Team.js
@@ -2,19 +2,19 @@ export const Team = [
     {
         title: 'ollionaire',
         url: '#',
-        desc: 'Ex-Head of Vis. Dev. at Universal Pictures. 20+ big-screen credits (Transformers 2-7, The Avengers, Thor, The Wolverine, and Terminator Genisys). Currently heads a studio, working with Boston Dynamics, Netflix, and Riot Games.',
+        desc: 'Ex-Head at Universal Pictures. 20+ big-screen credits (Transformers 2-7, The Avengers, Thor, The Wolverine, and Terminator Genisys). Currently heads a studio, working with Boston Dynamics, Netflix, and Riot Games. Only compatible with Windows 7.',
         twitter: "https://twitter.com/TheOllionaire"
     },
     {
         title: 'chromedoves',
         url: '#',
-        desc: 'Ex-Goldman Sachs investment banker. Playing with bubbles since I was 5. Laying down one brick at a time. Have fun with it – otherwise, what\'s the point?',
+        desc: 'Ex-Goldman Sachs investment banker. Still trying to get reservations at Dorsia. Do it because it’s fun - I’ve been playing with bubbles since I was 5.',
         twitter: "https://twitter.com/chromedoves"
     },
     {
         title: 'akilwrath',
         url: '#',
         desc: 'Ex-FAANG, startup engineer. Used to building systems, currently building a vibe. Mom does not approve.',
-        twitter: "https://twitter.com/Falias13"
+        twitter: "https://twitter.com/haxhield"
     },
 ]
